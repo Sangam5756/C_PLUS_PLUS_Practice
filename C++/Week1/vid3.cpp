0ff4c7647d058191add82872fcd70d59bb1a1302
SangamMundhe <sangammunde3@gmail.com>
2024-01-02 20:55:18 +0530

    Add files via upload

---
+++ C++/Week1/vid3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+/*This Program is created by Sangam Mundhe
+this 
+is a
+multi line 
+comments
+*/
+int main()
+{
+cout<<"Hello World:";
+int sum;
+sum=7;
+cout<<sum;
+return 0;
+}
